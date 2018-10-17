0d6a8aaaec4f351368c67ed911cd681901a69910
Daniel Bretzigheimer <dbretzigheimer95@gmail.com>
2018-10-17 15:30:51 -0400

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World</p>
+      </header>
+    </div>
+  );
+}
