a3fd229480674f6a0938d16b06d7eb1c73583e2e
Daniel Bretzigheimer <dbretzigheimer95@gmail.com>
2023-02-09 18:44:45 +0100

    Projects updated

--- src/components/projects/ProjectPreview.tsx
+++ src/components/projects/ProjectPreview.tsx
@@ -5,13 +5,17 @@ export function ProjectPreview() {
   return (
     <Box className="project-preview" display="flex" justifyContent="center" mt="5vmin" mb="5vmin">
       <BigProjectCard index={0}>
-        <img src="/images/projects/website.png" />
-      </BigProjectCard>
-      <BigProjectCard index={1}>
         <img src="/images/projects/hega.png" />
       </BigProjectCard>
-      <BigProjectCard index={2} />
-      <BigProjectCard index={3} />
+      <BigProjectCard index={1}>
+        <img src="/images/projects/website.png" />
+      </BigProjectCard>
+      <BigProjectCard index={2}>
+        <img src="/images/projects/mui.png" />
+      </BigProjectCard>
+      <BigProjectCard index={3}>
+        <img src="/images/projects/club.png" />
+      </BigProjectCard>
     </Box>
   );
 }
